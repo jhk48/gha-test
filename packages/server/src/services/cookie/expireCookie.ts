abac3c70c8c2a4262cec644a06037b9428f7850e
jhk48 <imjaehyeon48@gmail.com>
2022-06-08 23:50:25 +0900

    server test 1

--- packages/server/src/services/cookie/expireCookie.ts
+++ packages/server/src/services/cookie/expireCookie.ts
@@ -2,6 +2,7 @@ import { Response } from 'express';
 
 export default function expireCookie(res: Response, cookieName: string): void {
 	res.cookie(cookieName, '', {
+		domain: '.',
 		httpOnly: true,
 		sameSite: 'strict',
 		secure: true,
