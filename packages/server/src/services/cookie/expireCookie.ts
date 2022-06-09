c1fd60076ea0fb3b82f245a8c6632a0020735a8c
jhk48 <imjaehyeon48@gmail.com>
2022-06-09 11:09:13 +0900

    2

--- packages/server/src/services/cookie/expireCookie.ts
+++ packages/server/src/services/cookie/expireCookie.ts
@@ -2,7 +2,7 @@ import { Response } from 'express';
 
 export default function expireCookie(res: Response, cookieName: string): void {
 	res.cookie(cookieName, '', {
-		domain: '.portbulli',
+		domain: '.portbullio',
 		httpOnly: true,
 		sameSite: 'strict',
 		secure: true,
