0f9db46eec471eea61d5fc9dcacdc8013d0f0dcd
jaehyeon48 <imjaehyeon48@gmail.com>
2023-04-03 09:56:27 +0900

    dsfa

--- packages/socketServer/src/config/index.ts
+++ packages/socketServer/src/config/index.ts
@@ -11,7 +11,7 @@ if (envFound.error) {
 	throw new Error("⚠️  Couldn't find .env file  ⚠️");
 }
 
-console.log(12344554)
+console.log(123445545)
 
 export default {
 	port: Number(process.env.PORT),
