2fde8fd1159d35476b740b6f97e708efde498fbb
jaehyeon48 <imjaehyeon48@gmail.com>
2023-04-15 12:10:52 +0900

    as

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -78,17 +78,16 @@ Input JSON:
 ]
 `;
 
-const OPENAI_KEY = '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';
-
 async function askToChatGpt() {
 	const response = await fetch('https://api.openai.com/v1/completions', {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
-			Authorization: `Bearer ${OPENAI_KEY}`
+			Authorization: `Bearer ${process.env.OPENAI_API_KEY}`
 		},
 		body: JSON.stringify({
 			model: 'text-davinci-003',
+			temperature: 0,
 			prompt: CHAT_GPT_PROMPT
 		})
 	});
