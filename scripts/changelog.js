0c72818bc794f748d04356c5b9ada94ae29ced31
jaehyeon48 <imjaehyeon48@gmail.com>
2023-04-15 22:08:57 +0900

    sfad

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -190,7 +190,7 @@ Output markdown:
 function removeEmojis(str) {
 	const emojiRegex =
 		/[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{1F900}-\u{1F9FF}\u{1F1E0}-\u{1F1FF}\u{1F191}-\u{1F251}\u{FE0E}\u{FE0F}]+/gu;
-	return str.replace(emojiRegex, '');
+	return str.replace(emojiRegex, '').trim();
 }
 
 async function fetchPrsInMilestone() {
