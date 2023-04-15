eb1a50ff09671d62544f5867ecc543e9f66e3998
jaehyeon48 <imjaehyeon48@gmail.com>
2023-04-15 22:20:51 +0900

    sfad

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -189,8 +189,15 @@ Output markdown:
 
 function removeEmojis(str) {
 	const emojiRegex =
-		/[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{1F900}-\u{1F9FF}\u{1F1E0}-\u{1F1FF}\u{1F191}-\u{1F251}\u{FE0E}\u{FE0F}]+/gu;
-	return str.replace(emojiRegex, '').trim();
+		/([\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{1F900}-\u{1F9FF}\u{1F1E0}-\u{1F1FF}\u{1F191}-\u{1F251}\u{FE0E}\u{FE0F}]|[\u{1F3FB}-\u{1F3FF}][\u{1F9B0}-\u{1F9B3}])|([\u{200D}\u{FE0E}\u{FE0F}]|[\u{1F3FB}-\u{1F3FF}])/gu;
+	const emojiPresentationRegex = /\p{Emoji_Presentation}/gu;
+	const emojiModifierRegex = /\p{Emoji_Modifier_Base}\p{Emoji_Modifier}?/gu;
+
+	return str
+		.replace(emojiRegex, '')
+		.replace(emojiPresentationRegex, '')
+		.replace(emojiModifierRegex, '')
+		.trim();
 }
 
 async function fetchPrsInMilestone() {
